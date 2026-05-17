9e133c480b9c54da2e2425383c79b0bd9a64c8ed
MACBOOK PRO <macbookpro@MACBOOKs-MacBook-Pro.local>
2026-05-17 12:26:30 +0300

    change hero pictures

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -4,15 +4,15 @@ const u = (id: string, w = 1200, h = 1500) =>
   `https://images.unsplash.com/${id}?auto=format&fit=crop&w=${w}&h=${h}&q=80`;
 
 export const IMG = {
-  heroPortrait1: u("photo-1531123897727-8f129e1688ce", 800, 1000), // young leader
-  heroPortrait2: u("photo-1573496359142-b8d87734a5a2", 800, 1000), // confident woman
-  heroPortrait3: u("photo-1494790108377-be9c29b29330", 800, 1000), // smiling woman
-  heroPortrait4: u("photo-1500648767791-00dcc994a43e", 800, 1000), // confident man
-  mentorship: u("photo-1522202176988-66273c2fd55f", 1400, 1000),    // group meeting
+  heroPortrait1: u("photo-1531123414780-f74242c2b052", 800, 1000), // young leader
+  heroPortrait2: u("photo-1694286066858-462538cd9886", 800, 1000), // confident woman
+  heroPortrait3: u("photo-1694286066815-4e17284d61ab", 800, 1000), // smiling woman
+  heroPortrait4: u("photo-1522529599102-193c0d76b5b6", 800, 1000), // confident man
+  mentorship: u("photo-1637856794303-d864ce316444", 1400, 1000),    // group meeting
   workshop: u("photo-1543269865-cbf427effbad", 1400, 1000),         // workshop
   speaking: u("photo-1475721027785-f74eccf877e2", 1400, 1000),      // speaker stage
   cohort: u("photo-1517245386807-bb43f82c33c4", 1400, 1000),        // students laptop
-  growth: u("photo-1524178232363-1fb2b075b655", 1400, 1000),        // classroom
+  growth: u("photo-1739298061740-5ed03045b280", 1400, 1000),        // classroom
   community: u("photo-1529390079861-591de354faf5", 1400, 1000),     // community
   gallery1: u("photo-1559223607-a43c990c692c", 800, 1000),
   gallery2: u("photo-1517048676732-d65bc937f952", 800, 600),
